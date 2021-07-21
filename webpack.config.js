ac20d942fadb1ac2757b118b5813b2c48a6bbfb8
2bleO <Onesis.olea@gmail.com>
2021-07-21 19:30:07 -0400

    List Structure
    -Create an index.js file and set an array of some simple to do tasks.
    
    -Write function to iterate over the tasks array and populate an HTML list item element for each task.
    
    -On page load render the dynamically created list of tasks in the dedicated placeholder.
    
    -Create a style.css and set rules for the To Do List.

--- webpack.config.js
+++ webpack.config.js
@@ -6,25 +6,30 @@ module.exports = {
   entry: {
     index: './src/index.js',
   },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.join(__dirname, '/dist'),
+    clean: true,
+  },
   devtool: 'inline-source-map',
   devServer: {
     contentBase: './dist',
   },
- plugins: [
-   new HtmlWebpackPlugin({
-     template: './src/index.html',
-   }),
- ],
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
+  watchOptions: {
+    poll: true,
+    ignored: /node_modules/,
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
+        exclude: /node_modules/,
       },
     ],
   },
