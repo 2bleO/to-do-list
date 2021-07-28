36ddd523c132fb15857e4c7a29ec6c361c0db360
Marijan Brvar <marijan.brvar@gmail.com>
2021-07-28 23:07:50 +0200

    Add test for html elemnts

--- src/__test__/add_task.test.js
+++ src/__test__/add_task.test.js
@@ -1,12 +1,31 @@
+/**
+ * @jest-environment jsdom
+ */
+
 import Duty from '../task';
 
-describe('Add new task', () => {
-  test('', () => {
+describe('Add new Task', () => {
+  test('Create new Duty object', () => {
     const expected = new Duty('Task name', true, 3);
     const result = new Duty('Task name', true, 3);
 
-    console.log(result);
-
     expect(result).toEqual(expected);
+  });
+
+  test('Displays a task at the end of the list on enter key press in input', () => {
+    const li = document.querySelectorAll('LI');
+    const e = document.createEvent('Event');
+    e.initEvent('keyup', false, true);
+    document.getElementById('item').dispatchEvent(e);
+    expect(li.length).toBe(0);
+  });
+
+  test('Displays a task at the end of the list on enter key press in input', () => {
+    const li = document.querySelectorAll('LI');
+    li.innerText = 'Test Task';
+    const e = document.createEvent('Event');
+    e.initEvent('keyup', false, true);
+    document.getElementById('item').dispatchEvent(e);
+    expect(li.innerText).toBe('Test Task');
   });
 });
